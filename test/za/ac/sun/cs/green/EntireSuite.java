6032444c186582a70c65d58d09a8a07f20581a8b
Willem Visser <willem@gmail.com>
2018-07-23 18:31:28 +0200

    Update EntireSuite.java

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -36,7 +36,7 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class
-	//,SATZ3Test.class
+	,SATZ3Test.class
 })
 
 public class EntireSuite {
