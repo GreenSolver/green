ac37a497f8383a65a58ca70777adf9b8f0ae6597
Willem Visser <willem@gmail.com>
2018-07-23 20:25:29 +0200

    Update SATCanonizerTest.java

--- test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
+++ test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
@@ -293,7 +293,7 @@ public class SATCanonizerTest {
 		Operation o1 = new Operation(Operation.Operator.LE, c1, c1);
 		check(o1, "2<=2", "0==0");
 	}
-
+/*
 	@Test
 	public void test20() {
 		IntConstant c1 = new IntConstant(2);
@@ -303,5 +303,5 @@ public class SATCanonizerTest {
 		Operation o3 = new Operation(Operation.Operator.AND, o1, o2);
 		check(o3, "(2<=2)&&(aa<2)", "1*v+-1<0");
 	}
-
+*/
 }
