d559f7251abfde61cc6c704dc07dff251ff15092
Flora <flora-juliane.slezak@student.fh-kiel.de>
2018-07-30 15:54:38 +0200

    make the test passing again

--- test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
+++ test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
@@ -291,7 +291,7 @@ public class SATCanonizerTest {
 	public void test19() {
 		IntConstant c1 = new IntConstant(2);
 		Operation o1 = new Operation(Operation.Operator.LE, c1, c1);
-		check(o1, "2<=2", "0!=0");
+		check(o1, "2<=2", "0==0");
 	}
 
 	@Test
