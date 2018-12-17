8ef97da122baff37c2ec7a4f89236a3933f11fa7
Jaco Geldenhuys <jacogeld@gmail.com>
2018-12-17 20:49:03 +0200

    Improve support for real constraints [ci skip]

--- src/main/java/za/ac/sun/cs/green/service/z3/ModelZ3FloatService.java
+++ src/main/java/za/ac/sun/cs/green/service/z3/ModelZ3FloatService.java
@@ -30,9 +30,8 @@ public class ModelZ3FloatService extends ModelSMTLIBFloatService {
 		super(solver);
 		String DRIVE = new File("").getAbsolutePath() + "/";
 		String sub = "lib/z3/build/z3";
-		String z3Path = DRIVE+sub;
-		InputStream is = SATZ3Service.class.getClassLoader()
-				.getResourceAsStream("green/build.properties");
+		String z3Path = DRIVE + sub;
+		InputStream is = SATZ3Service.class.getClassLoader().getResourceAsStream("green/build.properties");
 		if (is != null) {
 			Properties p = new Properties();
 			try {
@@ -66,78 +65,88 @@ public class ModelZ3FloatService extends ModelSMTLIBFloatService {
 				long a = System.currentTimeMillis() - startTime;
 				timeConsumption += a;
 				unsatTimeConsumption += a;
+				stdin.close();
+				stdout.close();
+				process.destroy();
 				return null;
 			} else if (!output.equals("sat")) {
 				log.fatal("Z3 returned a null: " + output);
+				stdin.close();
+				stdout.close();
+				process.destroy();
 				return null;
 			}
 
 			stdin.write("(get-model)(exit)\n".getBytes());
 			stdin.flush();
 			stdin.close();
-            output = outReader.lines().collect(Collectors.joining());
+			output = outReader.lines().collect(Collectors.joining());
 			stdout.close();
 			process.destroy();
 
 			long a = System.currentTimeMillis() - startTime;
 			timeConsumption += a;
-			Map<Variable,Object> tmp = retrieveModel(output, variables);
+			Map<Variable, Object> tmp = retrieveModel(output, variables);
 			satTimeConsumption += a;
 			return tmp;
 		} catch (IOException x) {
 			log.log(Level.FATAL, x.getMessage(), x);
 		}
-        return null;
+		return null;
 	}
 
-    private Map<Variable, Object> retrieveModel(String output, Map<Variable, String> variables) {
+	private Map<Variable, Object> retrieveModel(String output, Map<Variable, String> variables) {
 		output = output.replaceAll("^\\s*\\(model\\s+(.*)\\s*\\)\\s*$", "$1@");
-        output = output.replaceAll("\\)\\s*\\(define-fun", ")@(define-fun");
-        output = output.replaceAll("\\(define-fun\\s+([\\w-]+)\\s*\\(\\)\\s*[\\w]+\\s+([^@]+)\\s*\\)@", "$1 == $2 ;; ");
+		output = output.replaceAll("\\)\\s*\\(define-fun", ")@(define-fun");
+		output = output.replaceAll("\\(define-fun\\s+([\\w-]+)\\s*\\(\\)\\s*[\\w]+\\s+([^@]+)\\s*\\)@", "$1 == $2 ;; ");
 		final Map<String, String> assignment = new HashMap<>();
 		for (String asgn : output.split(";;")) {
-		    if (asgn.contains("==")) {
-                String[] pair = asgn.split("==");
-                pair[1] = pair[1].trim();
-                if (pair[1].charAt(0) == ('(')) {
-                	pair[1] = pair[1].substring(1, pair[1].length()-1).trim();
-                	boolean negate = false;
-                	if (pair[1].charAt(0) == '-') {
-                		negate = true;
-                		pair[1] = pair[1].substring(1, pair[1].length()).trim();
-                		pair[1] = pair[1].substring(2, pair[1].length()-1).trim();
-                	} else {
-                		pair[1] = pair[1].substring(1, pair[1].length()).trim();
-                	}
-                	String[] nums = pair[1].split(" ");
-                	float numerator = Float.parseFloat(nums[0]);
-                	float denominator = Float.parseFloat(nums[1]);
-                	float fin = numerator/denominator;
-                	fin = negate? -fin : fin;
-                	pair[1] = fin + "";
-                }
-                assignment.put(pair[0].trim(), pair[1].trim());
-            }
-        }
+			if (asgn.contains("==")) {
+				String[] pair = asgn.split("==");
+				pair[1] = pair[1].trim();
+				if (pair[1].charAt(0) == ('(')) {
+					pair[1] = pair[1].substring(1, pair[1].length() - 1).trim();
+					boolean negate = false;
+					if (pair[1].charAt(0) == '-') {
+						negate = true;
+						pair[1] = pair[1].substring(1, pair[1].length()).trim();
+						pair[1] = pair[1].substring(2, pair[1].length() - 1).trim();
+					} else {
+						pair[1] = pair[1].substring(1, pair[1].length()).trim();
+					}
+					String[] nums = pair[1].split(" ");
+					float numerator = Float.parseFloat(nums[0]);
+					float denominator = Float.parseFloat(nums[1]);
+					float fin = numerator / denominator;
+					fin = negate ? -fin : fin;
+					pair[1] = fin + "";
+				}
+				assignment.put(pair[0].trim(), pair[1].trim());
+			}
+		}
 
 		HashMap<Variable, Object> model = new HashMap<>();
 		for (Map.Entry<Variable, String> entry : variables.entrySet()) {
-		    Variable var = entry.getKey();
-		    String name = entry.getValue();
-		    if (assignment.containsKey(name)) {
-                Constant value = null;
-                if (var instanceof IntVariable) {
-                	String val = assignment.get(name);
+			Variable var = entry.getKey();
+			String name = entry.getValue();
+			if (assignment.containsKey(name)) {
+				Constant value = null;
+				if (var instanceof IntVariable) {
+					String val = assignment.get(name);
 					val = val.replaceAll("\\(\\s*-\\s*(.+)\\)", "-$1");
 					value = new IntConstant(Integer.parseInt(val));
+				} else if (var instanceof IntegerVariable) {
+					String val = assignment.get(name);
+					val = val.replaceAll("\\(\\s*-\\s*(.+)\\)", "-$1");
+					value = new IntegerConstant(Long.parseLong(val));
 				} else if (var instanceof RealVariable) {
-                    value = new RealConstant(Double.parseDouble(assignment.get(name)));
-                }
-		        if (value != null) {
-                    model.put(var, value);
-                }
-            }
-        }
+					value = new RealConstant(Double.parseDouble(assignment.get(name)));
+				}
+				if (value != null) {
+					model.put(var, value);
+				}
+			}
+		}
 		return model;
 	}
 
