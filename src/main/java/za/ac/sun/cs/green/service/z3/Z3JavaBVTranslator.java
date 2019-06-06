0dc704b0a6b1d26fbbce3faf2f5d5a6ada6e3258
JHTaljaard <johannes.h.taljaard@gmail.com>
2019-06-06 17:22:23 +0200

    Updated BV translation

--- src/main/java/za/ac/sun/cs/green/service/z3/Z3JavaBVTranslator.java
+++ src/main/java/za/ac/sun/cs/green/service/z3/Z3JavaBVTranslator.java
@@ -6,29 +6,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
-import za.ac.sun.cs.green.expr.IntConstant;
-import za.ac.sun.cs.green.expr.IntVariable;
-import za.ac.sun.cs.green.expr.Operation;
-import za.ac.sun.cs.green.expr.RealConstant;
-import za.ac.sun.cs.green.expr.RealVariable;
-import za.ac.sun.cs.green.expr.Variable;
-import za.ac.sun.cs.green.expr.Visitor;
-import za.ac.sun.cs.green.expr.VisitorException;
-
-import com.microsoft.z3.ArithExpr;
-import com.microsoft.z3.BoolExpr;
-import com.microsoft.z3.Context;
-import com.microsoft.z3.Expr;
-import com.microsoft.z3.Z3Exception;
+import com.microsoft.z3.*;
+import za.ac.sun.cs.green.expr.*;
 
 class Z3JavaBVTranslator extends Visitor {
 	
 	private Context context = null;
-
 	private Stack<Expr> stack = null;
-	
 	private List<BoolExpr> domains = null;
-
 	private Map<Variable, Expr> v2e = null;
 
 	public Z3JavaBVTranslator(Context c) {
@@ -56,12 +41,11 @@ class Z3JavaBVTranslator extends Visitor {
 	public Map<Variable, Expr> getVariableMap() {
 		return v2e;
 	}
-	
 
 	@Override
-	public void postVisit(IntConstant constant) {			
+	public void postVisit(IntegerConstant constant) {
 		try {
-			stack.push(context.mkInt(constant.getValue()));
+			stack.push(context.mkBV(constant.getValue(), constant.getSize()));
 		} catch (Z3Exception e) {
 			e.printStackTrace();
 		}
@@ -76,38 +60,69 @@ class Z3JavaBVTranslator extends Visitor {
 		}
 	}
 
-	@Override
-	public void postVisit(IntVariable variable) {
-		Expr v = v2e.get(variable);
-		if (v == null) {
-			Integer lower = variable.getLowerBound();
-			Integer upper = variable.getUpperBound();
-			try {
-				v = context.mkIntConst(variable.getName());
-				// now add bounds
-				BoolExpr low  = context.mkGe((ArithExpr)v,(ArithExpr)context.mkInt(lower));
-				BoolExpr high = context.mkLe((ArithExpr)v,(ArithExpr)context.mkInt(upper));
-				domains.add(context.mkAnd(low,high));
-			} catch (Z3Exception e) {
-				e.printStackTrace();
-			}
-			v2e.put(variable, v);
-		}
-		stack.push(v);
-	}
+//	@Override
+//	public void postVisit(IntVariable variable) {
+//        BitVecExpr v = v2e.get(variable);
+//		if (v == null) {
+//			Integer lower = variable.getLowerBound();
+//			Integer upper = variable.getUpperBound();
+//			try {
+//				v = context.mkBV(variable.getName(), 0);
+//				// now add bounds
+//				BoolExpr low  = context.mkGe((ArithExpr)v,(ArithExpr)context.mkInt(lower));
+//				BoolExpr high = context.mkLe((ArithExpr)v,(ArithExpr)context.mkInt(upper));
+//				domains.add(context.mkAnd(low,high));
+//			} catch (Z3Exception e) {
+//				e.printStackTrace();
+//			}
+//			v2e.put(variable, v);
+//		}
+//		stack.push(v);
+//	}
+
+    @Override
+    public void postVisit(IntegerVariable variable) {
+        BitVecExpr v = (BitVecExpr) v2e.get(variable);
+        if (v == null) {
+            Long lower = variable.getLowerBound();
+            Long upper = variable.getUpperBound();
+            Integer size = variable.getSize();
+            try {
+                v = context.mkBVConst(variable.getName(), size);
+                // now add bounds
+                BoolExpr low  = context.mkBVSGE(v, context.mkBV(lower, size));
+                BoolExpr high = context.mkBVSLE(v, context.mkBV(upper, size));
+                domains.add(context.mkAnd(low,high));
+            } catch (Z3Exception e) {
+                e.printStackTrace();
+            }
+            v2e.put(variable, v);
+        }
+        stack.push(v);
+    }
 
 	@Override
 	public void postVisit(RealVariable variable) {
 		Expr v = v2e.get(variable);
 		if (v == null) {
-			int lower = (int) (double) variable.getLowerBound();
-			int upper = (int) (double) variable.getUpperBound();
+			Double lower = variable.getLowerBound();
+			Double upper = variable.getUpperBound();
+			int size = variable.getSize();
 			try {
-				v = context.mkRealConst(variable.getName());
-				// now add bounds
-				BoolExpr low  = context.mkGe((ArithExpr)v,(ArithExpr)context.mkReal(lower));
-				BoolExpr high = context.mkLe((ArithExpr)v,(ArithExpr)context.mkReal(upper));
-				domains.add(context.mkAnd(low,high));
+			    if (size == 32) {
+                    v = context.mkConst(variable.getName(), context.mkFPSort32());
+                    // now add bounds
+                    BoolExpr low  = context.mkFPGt((FPExpr) v, context.mkFP(lower, context.mkFPSort32()));
+                    BoolExpr high = context.mkFPLt((FPExpr) v, context.mkFP(upper, context.mkFPSort32()));
+                    domains.add(context.mkAnd(low,high));
+                } else {
+                    v = context.mkConst(variable.getName(), context.mkFPSortDouble());
+                    // now add bounds
+                    BoolExpr low  = context.mkFPGt((FPExpr) v, context.mkFP(lower, context.mkFPSortDouble()));
+                    BoolExpr high = context.mkFPLt((FPExpr) v, context.mkFP(upper, context.mkFPSortDouble()));
+                    domains.add(context.mkAnd(low,high));
+                }
+
 			} catch (Z3Exception e) {
 				e.printStackTrace();
 			}
@@ -119,7 +134,7 @@ class Z3JavaBVTranslator extends Visitor {
 	@Override
 	public void postVisit(Operation operation) throws VisitorException {
 		Expr l = null;
-		Expr r = null;
+        Expr r = null;
 		int arity = operation.getOperator().getArity();
 		if (arity == 2) {
 			if (!stack.isEmpty()) {
@@ -133,10 +148,12 @@ class Z3JavaBVTranslator extends Visitor {
 				l = stack.pop();
 			}
 		}
+		assert l != null;
+		assert r != null;
 		try {
 			switch (operation.getOperator()) {
-			case NOT: 
-				stack.push(context.mkNot((BoolExpr) l));
+			case NOT:
+                stack.push(context.mkBVNot((BitVecExpr) l));
 				break;
 			case EQ:
 				stack.push(context.mkEq(l, r));
@@ -145,37 +162,64 @@ class Z3JavaBVTranslator extends Visitor {
 				stack.push(context.mkNot(context.mkEq(l, r)));
 				break;
 			case LT:
-				stack.push(context.mkLt((ArithExpr) l, (ArithExpr) r));
+                stack.push(context.mkBVSLT((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			case LE:
-				stack.push(context.mkLe((ArithExpr) l, (ArithExpr) r));
+			    if (l instanceof ArithExpr && r instanceof ArithExpr) {
+                    stack.push(context.mkLe((ArithExpr) l, (ArithExpr) r));
+                } else if (l instanceof BoolExpr && r instanceof BitVecExpr) {
+//                    stack.push(context.mkLe((ArithExpr) l, (BitVecExpr) r));
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BoolExpr) {
+//                    stack.push(context.mkLe((BitVecExpr) l, (BitVecExpr) r));
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BitVecExpr) {
+                    stack.push(context.mkBVSLE((BitVecExpr) l, (BitVecExpr) r));
+                }
 				break;
 			case GT:
-				stack.push(context.mkGt((ArithExpr) l, (ArithExpr) r));
+				stack.push(context.mkBVSGT((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			case GE:
-				stack.push(context.mkGe((ArithExpr) l, (ArithExpr) r));
+                if (l instanceof ArithExpr && r instanceof ArithExpr) {
+                    stack.push(context.mkGe((ArithExpr) l, (ArithExpr) r));
+                } else if (l instanceof BoolExpr && r instanceof BitVecExpr) {
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BoolExpr) {
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BitVecExpr) {
+                    stack.push(context.mkBVSGE((BitVecExpr) l, (BitVecExpr) r));
+                }
 				break;
 			case AND:
-				stack.push(context.mkAnd((BoolExpr) l, (BoolExpr) r));
+			    if (l instanceof BoolExpr && r instanceof BoolExpr) {
+                    stack.push(context.mkAnd((BoolExpr) l, (BoolExpr) r));
+                } else if (l instanceof BoolExpr && r instanceof BitVecExpr) {
+//                    stack.push(context.mkBVAND((BitVecExpr) l, (BitVecExpr) r));
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BoolExpr) {
+                    System.out.println("##@!@");
+                } else if (l instanceof BitVecExpr && r instanceof BitVecExpr) {
+                    stack.push(context.mkBVAND((BitVecExpr) l, (BitVecExpr) r));
+                }
 				break;
 			case OR:
-				stack.push(context.mkOr((BoolExpr) l, (BoolExpr) r));
+				stack.push(context.mkBVOR((BitVecExpr) l, (BitVecExpr) r));
 				break;
-			case IMPLIES:
-				stack.push(context.mkImplies((BoolExpr) l, (BoolExpr) r));
-				break;
+//			case IMPLIES:
+//				stack.push(context.mkImplies(l, r));
+//				break;
 			case ADD:
-				stack.push(context.mkAdd((ArithExpr) l, (ArithExpr) r));
+				stack.push(context.mkBVAdd((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			case SUB:
-				stack.push(context.mkSub((ArithExpr) l, (ArithExpr) r));
+				stack.push(context.mkBVSub((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			case MUL:
-				stack.push(context.mkMul((ArithExpr) l, (ArithExpr) r));
+				stack.push(context.mkBVMul((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			case DIV:
-				stack.push(context.mkDiv((ArithExpr) l, (ArithExpr) r));
+				stack.push(context.mkBVSDiv((BitVecExpr) l, (BitVecExpr) r));
 				break;
 			default:
 				throw new TranslatorUnsupportedOperation(
